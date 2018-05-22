30f3bc9f32a5b047022808fe674d37773abbe8c7
nagelalexgit <nagel.alex.stop@gmail.com>
2018-05-22 23:29:19 +0100

    first commit

---
+++ Frewall Blocker/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Frewall_Blocker
+{
+    partial class FirewallBlocker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FirewallBlocker));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBlock = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnUnblock = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "APP PATH";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(84, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(520, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // btnBlock
+            // 
+            this.btnBlock.ForeColor = System.Drawing.Color.Maroon;
+            this.btnBlock.Location = new System.Drawing.Point(259, 65);
+            this.btnBlock.Name = "btnBlock";
+            this.btnBlock.Size = new System.Drawing.Size(75, 23);
+            this.btnBlock.TabIndex = 3;
+            this.btnBlock.Text = "Block";
+            this.btnBlock.UseVisualStyleBackColor = true;
+            this.btnBlock.Click += new System.EventHandler(this.btnBlock_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(615, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnUnblock
+            // 
+            this.btnUnblock.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnUnblock.Location = new System.Drawing.Point(383, 65);
+            this.btnUnblock.Name = "btnUnblock";
+            this.btnUnblock.Size = new System.Drawing.Size(75, 23);
+            this.btnUnblock.TabIndex = 5;
+            this.btnUnblock.Text = "Unblock";
+            this.btnUnblock.UseVisualStyleBackColor = true;
+            this.btnUnblock.Click += new System.EventHandler(this.btnUnblock_Click);
+            // 
+            // FirewallBlocker
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(705, 101);
+            this.Controls.Add(this.btnUnblock);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnBlock);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FirewallBlocker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firewall Blocker";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBlock;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnUnblock;
+    }
+}
+
---
+++ Frewall Blocker/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using NetFwTypeLib;
+using System.IO;
+
+namespace Frewall_Blocker
+{
+    public partial class FirewallBlocker : Form
+    {
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        public FirewallBlocker()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBlock_Click(object sender, EventArgs e)
+        {
+            FWRule(@txtPath.Text, NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT, NET_FW_ACTION_.NET_FW_ACTION_BLOCK, "1");
+        }
+        private void btnUnblock_Click(object sender, EventArgs e)
+        {
+            FWRule(@txtPath.Text, NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT, NET_FW_ACTION_.NET_FW_ACTION_BLOCK, "0");
+        }
+
+        void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+        void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] path = (string[])e.Data.GetData(DataFormats.FileDrop);
+            //if (path != null && path.Length != 0)
+            //{
+                txtPath.Text = path[0];
+            //}
+        }
+
+        private void FWRule(string path, NET_FW_RULE_DIRECTION_ d,
+        NET_FW_ACTION_ fwaction, string action)
+        {
+            try
+            {
+                INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(
+                Type.GetTypeFromProgID("HNetCfg.FWRule"));
+                firewallRule.Action = fwaction;
+                firewallRule.Enabled = true;
+                firewallRule.InterfaceTypes = "All";
+                firewallRule.ApplicationName = path;
+                firewallRule.Name = "Frewall_Blocker: " + Path.GetFileName(path);
+                INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance
+                (Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                firewallRule.Direction = d;
+                if (action == "1")
+                {
+                    firewallPolicy.Rules.Add(firewallRule);
+                    MessageBox.Show("Program is blocked");
+                }
+                else
+                {
+                    firewallPolicy.Rules.Remove(firewallRule.Name);
+                    MessageBox.Show("Program is unblocked");
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "ERROR"); }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                    string fullPath = openFileDialog1.FileName;
+                    txtPath.Text = fullPath;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
